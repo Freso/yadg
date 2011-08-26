4daa3307ef403f7a5267046e534406c72a7b4276
Slack06 <slack.06@mail.ru>
2011-08-26 16:16:15 +0200

    front end refinement

--- descgen/views.py
+++ descgen/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.template import Context
 from djcelery.models import TaskMeta
 import django.template.loader
+import json
 
 def index(request):
     if request.method == 'POST':
@@ -26,18 +27,26 @@ def index(request):
                 return HttpResponse(task.task_id)
             else:
                 return redirect('get_result',id=task.task_id)
+        elif request.GET.has_key("xhr"):
+            #we have an ajax request, but we are missing a required field
+            return HttpResponse(status=400)
     else:
         form = InputForm()
     return render(request,'index.html',{'form':form})
 
 def get_by_discogs_id(request,id):
     task = get_release_info.delay(int(id))
-    return redirect('get_result',id=task.task_id)
+    if request.GET.has_key("xhr"):
+        return HttpResponse(task.task_id)
+    else:
+        return redirect('get_result',id=task.task_id)
 
 def get_result(request,id):
     try:
         task = TaskMeta.objects.get(task_id__exact=id)
     except:
+        if request.GET.has_key("xhr"):
+            return HttpResponse(status=404)
         return render(request,'result_404.html',{'form':InputForm()}, status=404)
     if task.status == 'SUCCESS':
         (type,data) = task.result
@@ -45,12 +54,24 @@ def get_result(request,id):
             t = django.template.loader.get_template('result_template.txt')
             c = Context(data)
             result = t.render(c)
+            if request.GET.has_key("xhr"):
+                return HttpResponse(json.dumps(('result',result),ensure_ascii=False))
             return render(request,'result.html',{'result':result,'form':InputForm()})
         elif type == 'list':
+            if request.GET.has_key("xhr"):
+                for entry in data:
+                    entry['release'] = entry['release'].id
+                return HttpResponse(json.dumps(('list',data),ensure_ascii=False))
             return render(request,'result_list.html',{'release_list':data,'form':InputForm()})
         elif type == '404':
+            if request.GET.has_key("xhr"):
+                return HttpResponse(json.dumps(('notfound',[]),ensure_ascii=False))
             return render(request,'result_not_found_on_discogs.html', {'form':InputForm()})
     elif task.status == 'FAILURE' or task.status == 'REVOKED':
+        if request.GET.has_key("xhr"):
+            return HttpResponse(status=503)
         return render(request,'result_failed.html', {'form':InputForm()}, status=503)
     else:
+        if request.GET.has_key("xhr"):
+                return HttpResponse(json.dumps(('waiting',[]),ensure_ascii=False))
         return render(request,'result_waiting.html')
