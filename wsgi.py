9bfd8e71d937fc1f7468c69406d838b6996c894f
Slack06 <slack.06@mail.ru>
2011-08-30 03:25:39 +0200

    Revert "use deploy settings in wsgi app"
    This reverts commit 8984a8ad6762213cd6ed67d85e6f0f4498e3f74b.

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import site
 
 site.addsitedir('/home/yadg/djangoenv/lib/python2.6/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_deploy'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
