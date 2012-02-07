ce10417656b15821fc77702904a08c1cf2abdae4
Slack06 <slack.06@mail.ru>
2012-02-08 00:03:03 +0100

    width set to 820, responsive css removed

--- descgen/forms.py
+++ descgen/forms.py
@@ -3,8 +3,8 @@ from descgen.scraper.factory import SCRAPER_CHOICES,SCRAPER_DEFAULT
 from descgen.formatter import FORMAT_CHOICES,FORMAT_DEFAULT
 
 class InputForm(forms.Form):
-    input = forms.CharField(label='Search Term:',widget=forms.TextInput(attrs={'placeholder': 'Enter release url or search term','class':'input-xxlarge'}))
-    scraper = forms.ChoiceField(label='Scraper:', choices=SCRAPER_CHOICES, initial=SCRAPER_DEFAULT, widget=forms.Select(attrs={'class':'input-medium'}))
+    input = forms.CharField(label='Search Term:',widget=forms.TextInput(attrs={'placeholder': 'Enter release url or search term','class':'input-xhlarge'}))
+    scraper = forms.ChoiceField(label='Scraper:', choices=SCRAPER_CHOICES, initial=SCRAPER_DEFAULT, widget=forms.Select(attrs={'class':'auto_width'}))
 
 class FormatForm(forms.Form):
-    f = forms.ChoiceField(label='Format:', choices=FORMAT_CHOICES, initial=FORMAT_DEFAULT)
+    f = forms.ChoiceField(label='Format:', choices=FORMAT_CHOICES, initial=FORMAT_DEFAULT, widget=forms.Select(attrs={'class':'auto_width'}))
