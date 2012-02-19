94a9becd4f4b10a8ef985eefd87d191c5fce1c1e
Slack06 <slack.06@mail.ru>
2012-02-20 00:21:23 +0100

    new template tag for producing an artist list

---
+++ descgen/templatetags/wrapartists.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def wrapartists(artists, artist_format_string, separator, last_separator):
+    output = ''
+    artist_count = len(artists)
+    
+    i = 0
+    
+    for artist in artists:
+        output += artist_format_string % artist
+        
+        if i < artist_count - 2:
+            output += separator
+        elif i < artist_count - 1:
+            output += last_separator
+        
+        i += 1
+    
+    return output
