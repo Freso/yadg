79959de6eab9472c0a8af76297f812d991b2f230
Slack06 <slack.06@mail.ru>
2012-11-23 15:47:17 +0100

    add ScrapersView

--- descgen/urls.py
+++ descgen/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse
 from django.utils.functional import lazy
 
-from descgen.views import IndexView,ResultView,DownloadResultView,SettingsView
+from descgen.views import IndexView,ResultView,DownloadResultView,SettingsView,ScrapersView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^result/(?P<id>[\w\d-]+)$', ResultView.as_view(), name='get_result'),
     url(r'^result/(?P<id>[\w\d-]+)/(?P<format>[\w\d-]+)/(?P<title>.+?)\.txt$', DownloadResultView.as_view(), name='download_result'),
     url(r'^settings$', SettingsView.as_view(), name='settings'),
+    url(r'^available-scrapers$', ScrapersView.as_view(), name='scrapers_overview'),
     url(r'^api/$', RedirectView.as_view(url=reverse_lazy('api_v1_root'), permanent=False), name='api_root'),
     url(r'^api/v1/', include('descgen.api.v1_urls')),
     # url(r'^whatdesc/', include('whatdesc.foo.urls')),
