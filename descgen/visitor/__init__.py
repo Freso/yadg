48d8e64e53b599407647588fe801e5d86121e61b
Slack06 <slack.06@mail.ru>
2013-06-20 18:26:25 +0200

    add visitor base class

---
+++ descgen/visitor/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Visitor(object):
+    def visit(self, obj, *args, **kwargs):
+        meth = None
+        for cls in obj.__class__.__mro__:
+            meth_name = 'visit_'+cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
+
+        if not meth:
+            meth = self.generic_visit
+        return meth(obj, *args, **kwargs)
+
+    def generic_visit(self, node, *args, **kwargs):
+        raise NotImplementedError()
