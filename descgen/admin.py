5852a4ebb5fb7a0b05ca978542ade90b2ab3563a
Slack06 <slack.06@mail.ru>
2014-06-30 17:09:03 +0200

    display new Template model attribute in admin

--- descgen/admin.py
+++ descgen/admin.py
@@ -4,7 +4,7 @@ from .forms import TemplateForm
 
 class TemplateAdmin(admin.ModelAdmin):
     form = TemplateForm
-    list_display = ('__unicode__','is_public','is_default','depends_on', 'dependencies_set', 'cached_dependencies_set')
+    list_display = ('__unicode__', 'is_public', 'is_default', 'is_utility', 'depends_on', 'dependencies_set', 'cached_dependencies_set')
 
 admin.site.register(Template, TemplateAdmin)
 admin.site.register(Subscription)
