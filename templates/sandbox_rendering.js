a44e6be7214db65acabf6defa612cda3f6910674
Slack06 <slack.06@mail.ru>
2014-07-04 17:38:56 +0200

    move jSandbox include and sandbox rendering specific code into own files

---
+++ templates/sandbox_rendering.js
@@ -0,0 +1,15 @@
+var sandbox = new JSandbox(),
+    templates = {
+        {% for name,code in dependencies.items %}"{{ name|escapejs }}" : "{{ code|escapejs }}",
+        {%  endfor %}
+    },
+    template = "{{ template|escapejs }}",
+    data = {{ json_data|safe }},
+    eval_string = "myswig.render(input.template, { locals: input.data, filename: 'test' + (i++) })";
+
+sandbox.load("{{ STATIC_URL }}js/swig.min.js", function () { // onload
+    sandbox.load("{{ STATIC_URL }}js/swig.custom.js", function () {
+        this.exec({data: "var myswig = new swig.Swig({ loader: swig.loaders.memory(input.templates), autoescape: false }), i=0; yadg_filters.register_filters(myswig);", input: {templates: templates}});
+        this.eval(eval_string, success_callback, {template: template, data: data}, failure_callback);
+    });
+});
