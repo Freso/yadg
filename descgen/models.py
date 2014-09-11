5ab23b4c1acbc058900957ada63d29bd2699d820
Slack06 <slack.06@mail.ru>
2014-09-11 20:19:38 +0200

    add parameter to templates_for_user method of Template model to choose whether to prefetch the owner of the templates and use the new parameter when appropriate

--- descgen/models.py
+++ descgen/models.py
@@ -92,7 +92,7 @@ class Template(models.Model):
             raise ValidationError('One of the dependencies has a dependency on this template.')
 
     @staticmethod
-    def templates_for_user(user, with_utility=True, sort_by_name=False):
+    def templates_for_user(user, with_utility=True, sort_by_name=False, prefetch_owner=False):
         if user is not None and user.is_authenticated():
             subscribed_to = user.subscribed_to.values('user_id').distinct()
             t = Template.objects.filter(Q(owner__in=subscribed_to, is_public__exact=True) | Q(owner__exact=user.pk) | Q(is_default__exact=True))
@@ -100,6 +100,8 @@ class Template(models.Model):
             t = Template.objects.filter(is_default__exact=True)
         if not with_utility:
             t = t.filter(is_utility__exact=False)
+        if prefetch_owner:
+            t = t.select_related('owner')
         if sort_by_name:
             t = t.extra(select={'lower_name': 'lower(name)'}).order_by('lower_name')
         return t
